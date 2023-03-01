c7d0b209d67e1140a5830a7a67cb7613bc596930
leezy1017 <burgoschris83@yahoo.com>
2023-03-01 04:27:34 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -11,6 +11,10 @@ public class Lab {
      * @return the sum of a and b.
      */
     public int addNumbers(int a, int b){
-        return 0;
+       
+        
+
+        return a + b;
+
     }
 }
